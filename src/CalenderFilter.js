be531b1381cc6425b7c79856f548593e97ab5ee2
Ehab Mohamed <ehab.mohamed@argaam.com>
2023-01-19 15:35:46 +0200

    update calendar

--- src/CalenderFilter.js
+++ src/CalenderFilter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 // to call data
 import axios from "axios";
 import AccessRefreshTokens from "./RefreshToken/AccessRefreshTokens";
@@ -13,35 +13,47 @@ import Paper from "@mui/material/Paper";
 import Moment from "moment";
 // style
 import "./Test.css";
-
+// Calender Style
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import "./Calender.css";
 // Calender Init
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import { LocalizationProvider } from "@mui/x-date-pickers-pro";
-import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
-import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import { DateRange } from "react-date-range";
+import format from "date-fns/format";
+import { addDays } from "date-fns";
 
 const FinancialRatios = () => {
   const [historicalPrice, setHistoricalPrice] = useState([]);
-  const [startDate, setStartDate] = useState("2022-10-18");
-  const [endDate, setEndDate] = useState("2022-10-25");
-  const [value, setValue] = useState([startDate, endDate]);
 
-  const formatDate = (date) => Moment(date).format("DD/MM/YYYY");
+  // Date state
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+  const [openCalendar, setOpenCalendar] = useState(false);
+  const refOne = useRef(null);
+
+  const formatDate = (date) => Moment(date).format("YYYY-MM-DD");
   const SignRemove = (num) => Math.abs(num).toFixed(2);
   const formatNum = (num) => {
     let number = num;
     return number.toLocaleString(num);
   };
 
+  let startDate = formatDate(range[0].startDate);
+  let endDate = formatDate(range[0].endDate);
+
+  // console.log(formatDate(state[0].startDate).split("/").join("-"));
+
   useEffect(() => {
     AccessRefreshTokens.getAccessToken();
 
     axios
       .get(
-        `https://data.argaam.com/api/v1/json/ir-api/chart-data-table/${startDate
-          .split("/")
-          .join("-")}/${endDate.split("/").join("-")}`,
+        `https://data.argaam.com/api/v1/json/ir-api/chart-data-table/${startDate}/${endDate}`,
         {
           headers: {
             authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -57,32 +69,56 @@ const FinancialRatios = () => {
       });
   }, [startDate, endDate, localStorage.getItem("token")]);
 
+  useEffect(() => {
+    document.addEventListener("keydown", hideOnEscape, true);
+    document.addEventListener("click", hideOnClickOutside, true);
+  }, []);
+
+  // Hide Calendar on ESC press
+  const hideOnEscape = (e) => {
+    if (e.key === "Escape") {
+      setOpenCalendar(false);
+    }
+  };
+  // Hide Calendar on outside click
+  const hideOnClickOutside = (e) => {
+    console.log(refOne.current);
+    if (refOne.current && !refOne.current.contains(e.target)) {
+      setOpenCalendar(false);
+    }
+  };
+  // console.log(formatDate(range[0].startDate));
+
+  console.log(startDate, endDate);
   return (
     <>
       <div className="container-md">
         <div className="historical-price py-5">
           <h2>HISTORICAL PRICE</h2>
-          <LocalizationProvider
-            dateAdapter={AdapterDayjs}
-            localeText={{ start: "Start-Date", end: "End-Date" }}
-          >
-            <DateRangePicker
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              renderInput={(startProps, endProps) => (
-                <>
-                  {setStartDate(startProps.inputProps.value)}
-                  {setEndDate(endProps.inputProps.value)}
 
-                  <TextField {...startProps} />
-                  <Box sx={{ mx: 2 }}> to </Box>
-                  <TextField {...endProps} />
-                </>
-              )}
+          <div className="calendarWrap">
+            <input
+              value={`${formatDate(range[0].startDate)} to ${formatDate(
+                range[0].endDate
+              )}`}
+              readOnly
+              className="inputBox"
+              onClick={() => setOpenCalendar((open) => !open)}
             />
-          </LocalizationProvider>
+            <div ref={refOne}>
+              {openCalendar && (
+                <DateRange
+                  onChange={(item) => setRange([item.selection])}
+                  showSelectionPreview={true}
+                  moveRangeOnFirstSelection={false}
+                  months={2}
+                  ranges={range}
+                  direction="horizontal"
+                />
+              )}
+            </div>{" "}
+          </div>
+
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
